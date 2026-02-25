dc2375431d7b1ae2d05e91de34e8c89f9c587879
Dmytro Stebaiev <dimastebaev@gmail.com>
2026-02-25 18:47:02 +0200

    Add getRequiredNodesNumber

--- contracts/IBountyV2.sol
+++ contracts/IBountyV2.sol
@@ -44,4 +44,5 @@ interface IBountyV2 {
     function estimateBounty(uint nodeIndex) external view returns (uint);
     function getNextRewardTimestamp(uint nodeIndex) external view returns (uint);
     function getEffectiveDelegatedSum() external view returns (uint[] memory);
+    function getRequiredNodesNumber(uint256 delegatedValue) external view returns (uint256 requiredNodesNumber);
 }
